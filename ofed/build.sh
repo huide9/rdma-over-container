6802ea733f17305320e28fae186a2d1d444e7d01
huide9@gmail.com <huide9@gmail.com>
2022-10-18 00:38:01 -0700

    Dockerfile for rdma container

--- ofed/build.sh
+++ ofed/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t rdma:ofed-cuda-cudnn-pytorch1.8-with-ompi5.0rc-vision0.9 - < ./Dockerfile
+docker build -t rdma:ofed-cuda102-cudnn8-torch1.8-ompi5.0rc-vision0.9 - < ./Dockerfile
